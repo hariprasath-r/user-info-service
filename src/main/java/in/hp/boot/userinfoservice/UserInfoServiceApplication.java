3ca748a01a8d5cd3eb25f5015104c38d51a4c1ad
Hariprasath <balaprasad24@gmail.com>
2020-05-01 11:23:38 +0530

    Temporarily Commenting out Eureka

--- src/main/java/in/hp/boot/userinfoservice/UserInfoServiceApplication.java
+++ src/main/java/in/hp/boot/userinfoservice/UserInfoServiceApplication.java
@@ -2,9 +2,8 @@ package in.hp.boot.userinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class UserInfoServiceApplication {
 
