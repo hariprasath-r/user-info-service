1b1f9e66640363e08854705daa82c3b9a4dbe694
Hariprasath <balaprasad24@gmail.com>
2020-05-01 12:14:55 +0530

    Added Swagger & Optimized Imports

--- src/main/java/in/hp/boot/userinfoservice/UserInfoServiceApplication.java
+++ src/main/java/in/hp/boot/userinfoservice/UserInfoServiceApplication.java
@@ -2,9 +2,10 @@ package in.hp.boot.userinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class UserInfoServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/in/hp/boot/userinfoservice/controllers/UserController.java
+++ src/main/java/in/hp/boot/userinfoservice/controllers/UserController.java
@@ -5,7 +5,6 @@ import in.hp.boot.userinfoservice.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
